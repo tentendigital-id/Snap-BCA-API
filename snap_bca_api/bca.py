8155fbf7904c6203e17e8cce4681ecca718a1d19
nurfitriyanto <nurfitriyanto13@gmail.com>
2024-11-14 11:23:40 +0700

    add requirements & timeout request

--- snap_bca_api/bca.py
+++ snap_bca_api/bca.py
@@ -1,13 +1,17 @@
 import base64
 import random
-import requests, datetime, hashlib, hmac, json
+import requests
+import hashlib
+import hmac
+import json
 from OpenSSL import crypto
 from datetime import datetime, timedelta, timezone
 
 
-class BCA_SNAP():
-
-    def __init__(self, client_id, client_secret, private_key, channel_id, partner_id, host="https://sandbox.bca.co.id", debug=False):
+class BCA_SNAP:
+    def __init__(
+        self, client_id, client_secret, private_key, channel_id, partner_id, host="https://sandbox.bca.co.id", debug=False
+    ):
         self.client_id = client_id
         self.client_secret = client_secret
         self.private_key = private_key
@@ -17,10 +21,10 @@ class BCA_SNAP():
         self.debug = debug
 
     def __getTimestamp(self):
-        return datetime.now(timezone(timedelta(hours=7))).isoformat(timespec='seconds')
+        return datetime.now(timezone(timedelta(hours=7))).isoformat(timespec="seconds")
 
     def __getRandomInt(self):
-        random_number = ''.join([str(random.randint(0, 9)) for _ in range(16)])
+        random_number = "".join([str(random.randint(0, 9)) for _ in range(16)])  # nosec
         return random_number
 
     def __getSnapAsymetricSignature(self, timestamp):
@@ -31,37 +35,29 @@ class BCA_SNAP():
         data_base64 = base64.b64encode(sign)
         return data_base64
 
-    def __getSnapSymetricSignature(self, method, path, access_token, timestamp, request_body=''):
+    def __getSnapSymetricSignature(self, method, path, access_token, timestamp, request_body=""):
         secret = self.client_secret.encode()
-        request_body_hash = hashlib.sha256(
-            request_body.encode()
-        ).hexdigest().lower()
+        request_body_hash = hashlib.sha256(request_body.encode()).hexdigest().lower()
 
-        StringToSign = (
-            method + ':' +
-            path + ':' +
-            access_token + ':' +
-            request_body_hash + ':' +
-            timestamp
-        ).encode()
+        StringToSign = (method + ":" + path + ":" + access_token + ":" + request_body_hash + ":" + timestamp).encode()
 
         signature = hmac.new(secret, StringToSign, digestmod=hashlib.sha512)
         return base64.b64encode(signature.digest()).decode()
 
     def __getSnapAccessToken(self, timestamp):
         headers = {
-            'Content-Type': 'application/json',
-            'X-TIMESTAMP': timestamp,
-            'X-CLIENT-KEY': self.client_id,
-            'X-SIGNATURE': self.__getSnapAsymetricSignature(timestamp)
+            "Content-Type": "application/json",
+            "X-TIMESTAMP": timestamp,
+            "X-CLIENT-KEY": self.client_id,
+            "X-SIGNATURE": self.__getSnapAsymetricSignature(timestamp),
         }
 
-        payload = {'grantType': 'client_credentials'}
-        r = requests.post(self.host + '/openapi/v1.0/access-token/b2b', headers=headers, data=json.dumps(payload))
+        payload = {"grantType": "client_credentials"}
+        r = requests.post(self.host + "/openapi/v1.0/access-token/b2b", headers=headers, data=json.dumps(payload), timeout=30)
         return r.json()
 
     def getBalance(self, account_number, partnerReferenceNo, external_id=""):
-        path = '/openapi/v1.0/balance-inquiry'
+        path = "/openapi/v1.0/balance-inquiry"
 
         if external_id == "":
             external_id = self.__getRandomInt()
@@ -79,10 +75,8 @@ class BCA_SNAP():
             "accountNo": account_number,
         }
 
-        data = json.dumps(payload, separators=(',', ':'))
-        signature = self.__getSnapSymetricSignature(
-            'POST', path, accessToken, timestamp, data
-        )
+        data = json.dumps(payload, separators=(",", ":"))
+        signature = self.__getSnapSymetricSignature("POST", path, accessToken, timestamp, data)
 
         header = {
             "Content-Type": "application/json",
@@ -94,16 +88,16 @@ class BCA_SNAP():
             "X-EXTERNAL-ID": external_id,
         }
 
-        r = requests.post(self.host + path, headers=header, data=data)
+        r = requests.post(self.host + path, headers=header, data=data, timeout=30)
         if self.debug:
             print("---- DEBUG RESPONSE HEADER ----")
             print(r.headers)
             print("---- --------------------- ----")
 
-        return r.content.decode()
+        return json.loads(r.content.decode())
 
     def getStatement(self, account_number, partnerReferenceNo, fromDateTime, toDateTime, external_id=""):
-        path = '/openapi/v1.0/bank-statement'
+        path = "/openapi/v1.0/bank-statement"
 
         if external_id == "":
             external_id = self.__getRandomInt()
@@ -123,10 +117,8 @@ class BCA_SNAP():
             "toDateTime": toDateTime,
         }
 
-        data = json.dumps(payload, separators=(',', ':'))
-        signature = self.__getSnapSymetricSignature(
-            'POST', path, accessToken, timestamp, data
-        )
+        data = json.dumps(payload, separators=(",", ":"))
+        signature = self.__getSnapSymetricSignature("POST", path, accessToken, timestamp, data)
 
         header = {
             "Content-Type": "application/json",
@@ -137,10 +129,10 @@ class BCA_SNAP():
             "X-SIGNATURE": signature,
             "X-EXTERNAL-ID": external_id,
         }
-        r = requests.post(self.host + path, headers=header, data=data)
+        r = requests.post(self.host + path, headers=header, data=data, timeout=30)
         if self.debug:
             print("---- DEBUG RESPONSE HEADER ----")
             print(r.headers)
             print("---- --------------------- ----")
 
-        return r.content.decode()
+        return json.loads(r.content.decode())
